1c60847903e905b2564bedb1dbaedad03a83db31
Agroscopic <hugo.lehoux@openmailbox.org>
2015-09-02 12:21:06 +0200

    Socialized website : Confidentiality policy, Follow on Fb, GitHub, link to our website agriculturefamiliale.com

--- src/AppBundle/Controller/CoreController.php
+++ src/AppBundle/Controller/CoreController.php
@@ -15,4 +15,12 @@ class CoreController extends Controller
     {
         return $this->render('AppBundle:Core:index.html.twig');
     }
+
+    /**
+     * @Route("/politique-de-confidentialite", name="confidentiality_policy")
+     */
+    public function confidentialityPolicyAction(Request $request)
+    {
+        return $this->render('AppBundle:Core:confidentiality-policy.html.twig');
+    }
 }
