93b76e91c23cb279aaffc51c63bd88f9b9176ccf
Agroscopic <hugo.lehoux@openmailbox.org>
2015-09-02 15:13:07 +0200

    Users : Downgraded to FOSUB1.3
    Current version of SonataUserBundle doesn’t accept FOSUB2.0

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,7 +23,6 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
-            new Sonata\UserBundle\SonataUserBundle(),
             new AppBundle\AppBundle(),
         );
 
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,25 +2,25 @@
 
 namespace AppBundle\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
+use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
-* @ORM\Entity
-* @ORM\Table(name="fos_user")
-*/
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
 class User extends BaseUser
 {
-/**
-* @ORM\Id
-* @ORM\Column(type="integer")
-* @ORM\GeneratedValue(strategy="AUTO")
-*/
-protected $id;
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
-public function __construct()
-{
-parent::__construct();
-// your own logic
-}
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
 }
